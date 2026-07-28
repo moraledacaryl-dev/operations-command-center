7ca8bcbb48c23e8fe9a1de5315c96eba53d0213d
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-07-28 16:15:24 +0800

    Protect uploaded file delivery

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ from .routers.api import router
 from .routers.integrations_v2 import router as integrations_v2_router
 from .security import load_security_settings, validate_security_settings
 from .seed import backfill_local_user_passwords, ensure_bootstrap_owner, seed_if_empty
+from .upload_access import UploadAccessMiddleware
 from .upload_safety import UploadSafetyMiddleware
 
 
@@ -38,7 +39,7 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(
     title="Manager Operations Command Center",
-    version="3.7.0",
+    version="3.8.0",
     lifespan=lifespan,
     docs_url="/docs" if security.expose_api_docs else None,
     redoc_url="/redoc" if security.expose_api_docs else None,
@@ -62,6 +63,7 @@ app.add_middleware(IdentityBoundaryMiddleware)
 app.add_middleware(RoleBoundaryMiddleware)
 app.add_middleware(DecisionBoundaryMiddleware)
 app.add_middleware(UploadSafetyMiddleware)
+app.add_middleware(UploadAccessMiddleware)
 
 
 @app.middleware("http")
